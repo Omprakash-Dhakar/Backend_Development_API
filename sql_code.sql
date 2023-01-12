b9c5610020a50cf8feb7515a6ddb8edfdd9b221a
OMPRAKASH DHAKAR <115010509+Omprakash-Dhakar@users.noreply.github.com>
2023-01-12 16:23:01 +0530

    Initial commit

--- sql_code.sql
+++ sql_code.sql
@@ -1,11 +1,13 @@
 # SQL Query for creating Tables and Triggers
 
+# Query for creating 'messages' table
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     text TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
 
+# Query for creating 'likes' table
 CREATE TABLE likes (
     id SERIAL PRIMARY KEY,
     message_id INTEGER NOT NULL,
@@ -13,6 +15,7 @@ CREATE TABLE likes (
     FOREIGN KEY (message_id) REFERENCES messages (id)
 );
 
+# Query for making a trigger function
 CREATE OR REPLACE FUNCTION update_likes_count()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -21,11 +24,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+# Calling trigger function when their is a like
 CREATE TRIGGER update_likes_count_on_like
 AFTER INSERT ON likes
 FOR EACH ROW
 EXECUTE FUNCTION update_likes_count();
 
+# calling trigger function for unlike
 CREATE TRIGGER update_likes_count_on_unlike
 AFTER DELETE ON likes
 FOR EACH ROW
