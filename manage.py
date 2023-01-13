15ded7b5d5756cdf81e88388235b638644ecebd9
OMPRAKASH DHAKAR <115010509+Omprakash-Dhakar@users.noreply.github.com>
2023-01-13 22:57:13 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/media_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgrespassword@localhost:5432/postgres'
 db = SQLAlchemy(app)
 
 
